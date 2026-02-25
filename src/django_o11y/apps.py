06a293e635734fb090a4ffa1149aa3e9f74e9513
adinhodovic <hodovicadin@gmail.com>
2026-02-26 00:58:28 +0100

    chore: Bump version

--- src/django_o11y/apps.py
+++ src/django_o11y/apps.py
@@ -81,7 +81,7 @@ class DjangoO11yConfig(AppConfig):
             try:
                 pkg_version = version("django-o11y")
             except PackageNotFoundError:
-                pkg_version = "0.2.12"
+                pkg_version = "0.3.0"
 
             banner = [
                 "",
